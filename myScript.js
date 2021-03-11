95df5dc32eb135765f37e24df9decd1ad7af4c4e
AkshayBura <80462379+AkshayBura@users.noreply.github.com>
2021-03-11 23:09:13 +0530

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,11 @@
+function promtMessage()
+{
+     var a = confirm("Enjoying the session so far?");
+     if(a) {
+               alert("Hope U Keep up the good work!");
+               }
+      else {
+                alert("If not still need to reach the finish line!")
+               }
+}      
+    
